326107e51778ece0f7af1900e2d21fd4a283aa71
ayush18pop <ayushsp05@gmail.com>
2025-02-11 21:35:20 +0530

    backend and frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <Button variant="outline" >BreakItDown</Button>
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter >
+        <p className='flex-auto'>Card Footer</p>
+      </CardFooter>
+  </Card>
+  </>
   )
 }
 
